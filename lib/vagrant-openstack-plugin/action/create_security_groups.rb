11c0f703cb1e387fe87740debe4a8e1d5aa3f26c
Tejinder Jheeta <tjheeta@ubuntu.(none)>
2014-04-08 19:03:25 -0700

    Use the tenant_id to filter the security_groups. Use string instead of symbol

--- lib/vagrant-openstack-plugin/action/create_security_groups.rb
+++ lib/vagrant-openstack-plugin/action/create_security_groups.rb
@@ -22,25 +22,31 @@ module VagrantPlugins
 
           # This loop creates all the security groups needed first
           # Use the cached security groups and update them if a new one is created
-          current_security_groups = network.list_security_groups
+          current_tenant_id = network.instance_variable_get("@current_tenant")["id"]
+          current_security_groups = network.list_security_groups(:tenant_id => current_tenant_id)
           security_group_rules.each do |rule| 
-              secgroup = current_security_groups.data[:body]['security_groups'].select { |v| v['name'] == rule[:name] }.first rescue nil
+              secgroup = current_security_groups.data[:body]['security_groups'].select { |v| v['name'] == rule["name"] }.first rescue nil
               if secgroup.nil?
-                  openstack.create_security_group(rule[:name], "")
-                  current_security_groups = network.list_security_groups
+                  #openstack.create_security_group(rule["name"], "")
+                  network.create_security_group(:name => rule["name"])
+                  current_security_groups = network.list_security_groups(:tenant_id => current_tenant_id)
               end
           end
-              
+
           # This group creates the actual rules as part of the security_group
-          security_group_rules.each do |rule| 
-              secgroup = current_security_groups.data[:body]['security_groups'].select { |v| v['name'] == rule[:name] }.first
+          security_group_rules.each do |rule|
+              secgroup = current_security_groups.data[:body]['security_groups'].select { |v| v['name'] == rule["name"] }.first
               # The rules may reference other groups to allow access to instead of cidr
               remote_group_ids = []
-              if rule[:remote_groups]
-                  rule[:remote_groups].each do |s|
-                      puts s
-                      tmp=current_security_groups.data[:body]['security_groups'].select { |v| v['name'] == s }.first
-                      remote_group_ids.push(tmp["id"])
+              if rule["remote_groups"]
+                  rule["remote_groups"].each do |s|
+                      begin 
+                          tmp=current_security_groups.data[:body]['security_groups'].select { |v| v['name'] == s }.first
+                          remote_group_ids.push(tmp["id"])
+                      rescue
+                          pp "The remote group #{s} that was specified in #{rule} doesn't exist. Was it created?"
+                          raise Errors::VagrantOpenStackError
+                      end
                   end
               else
                   remote_group_ids.push(nil)
@@ -51,15 +57,19 @@ module VagrantPlugins
                       pp network.create_security_group_rule(
                       secgroup["id"],
                       "ingress",
-                      :port_range_min => rule[:port_range_min],
-                      :port_range_max => rule[:port_range_max],
-                      :protocol => rule[:protocol],
-                      :remote_ip_prefix => rule[:remote_ip_prefix] ? rule[:remote_ip_prefix] : nil,
-                      :remote_group_id => remote_group_id ? remote_group_id : nil
-                      ) 
+                      :port_range_min => rule["port_range_min"],
+                      :port_range_max => rule["port_range_max"],
+                      :protocol => rule["protocol"],
+                      :remote_ip_prefix => rule["remote_ip_prefix"] ? rule["remote_ip_prefix"] : nil,
+                      :remote_group_id => remote_group_id ? remote_group_id : nil,
+                      :tenant_id => current_tenant_id
+                      )
                   rescue Excon::Errors::Conflict
-                      pp "Rule already made:"
-                      pp rule
+                      pp "Rule already made: #{rule}"
+                      #pp rule
+                      #pp secgroup["id"]
+                      #pp secgroup["name"]
+                      #pp current_tenant_id
                   end
               end
           end
